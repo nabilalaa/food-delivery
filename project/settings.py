f936aa253097dbb07e422e6a0911fe678d233154
nabil alaa <nabilalaa06@gmail.com>
2024-10-21 21:21:25 +0300

    food-delivery

--- project/settings.py
+++ project/settings.py
@@ -94,11 +94,31 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres.qfqqqhinhmxffkklhaof',
+
+        'PASSWORD': 'Ea28mFmUJXXvt3EN',
+
+        'HOST': 'aws-0-us-east-1.pooler.supabase.com',
+
+        'PORT': '6543',
+
     }
+
 }
 
 
