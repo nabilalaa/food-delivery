54772314234b719a80c33e1bfdbabcbbd284e262
nabilalaa <nabilalaa06@gmail.com>
2025-09-23 02:59:06 +0300

    food-delivery

--- project/settings.py
+++ project/settings.py
@@ -95,23 +95,23 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "postgres",
-#         "USER": "postgres.qfqqqhinhmxffkklhaof",
-#         "PASSWORD": "Ea28mFmUJXXvt3EN",
-#         "HOST": "aws-0-us-east-1.pooler.supabase.com",
-#         "PORT": "6543",
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "neondb",
+        "USER": "neondb_owner",
+        "PASSWORD": "npg_7AaUNVquTQl9",
+        "HOST": "ep-raspy-union-ag3scqw8-pooler.c-2.eu-central-1.aws.neon.tech",
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
